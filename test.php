5fc9fad4ae846612facfd9f82a54c4e76b3c1bf9
LGgameLAB <45958481+LGgameLAB@users.noreply.github.com>
2020-01-05 13:05:34 -0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <title> php test
+    </title>
+	<script src="jquery-3.4.1.min.js"></script>
+	<?php
+		$myfile = fopen("data.txt", "w") or die("Unable to open file!");
+		$txt = "Mickey Mouse\n";
+		fwrite($myfile, $txt);
+		$txt = "Minnie Mouse\n";
+		fwrite($myfile, $txt);
+		fclose($myfile);
+	?>
+	<script>
+		function readFile() {
+	 		jQuery.get("data.txt", function(txt){
+				$('#htmlText').text(txt);
+			});
+		};
+	  
+	 </script>
+  </head>
+  <body onload="readFile();">
+      <h5 id = "htmlText">
+        
+      </h5>
+      <h1>
+	      lol
+      </h1>
+	  
+  </body>
+</html>
